5c3ce39c0014847e61b39762d66a7083f54f3623
rimac9 <rimacmz@gmail.com>
2023-07-18 12:26:27 -0300

    clase 9: Mostrando y ocultando secciones

--- src/main.js
+++ src/main.js
@@ -1,50 +1,48 @@
-const api = axios.create({ // instancia de axios
-    baseURL: 'https://api.themoviedb.org/3/',
-    headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-    },
-    params: { // un objeto con parametros
-        'api_key': API_KEY,
-    },
-
+const api = axios.create({
+  // instancia de axios
+  baseURL: "https://api.themoviedb.org/3/",
+  headers: {
+    "Content-Type": "application/json;charset=utf-8",
+  },
+  params: {
+    // un objeto con parametros
+    api_key: API_KEY,
+  }
 });
-// utilizamos la instancia de axios ára cada uno de nuestras soli
+// utilizamos la instancia de axios pára cada uno de nuestras soli
 
 async function getTrendingMoviesPreview() {
-  const {data} = await api("trending/movie/day");
-  const movies = data.results;
-  // console.log({data, movies})
-  movies.forEach((movie) => {
+    const { data } = await api("trending/movie/day");
+    const movies = data.results;
+    // console.log({data, movies})
+    movies.forEach((movie) => {
     //                                         seleccionamos d section '#' dentro del secction la clase
-    const trendingPreviewMoviesContainer = document.querySelector(
-      "#trendingPreview .trendingPreview-movieList"
-    );
-
+    const trendingPreviewMoviesContainer = document.querySelector("#trendingPreview .trendingPreview-movieList");
     const movieCotainer = document.createElement("div");
     movieCotainer.classList.add("movie-container");
     const movieImg = document.createElement("img");
     movieImg.classList.add("movie-img");
     movieImg.setAttribute("alt", movie.title);
-    movieImg.setAttribute(
-      "src",
-      "https://image.tmdb.org/t/p/w300" + movie.poster_path
-    );
+    movieImg.setAttribute("src", "https://image.tmdb.org/t/p/w300" + movie.poster_path);
 
     movieCotainer.appendChild(movieImg);
     trendingPreviewMoviesContainer.appendChild(movieCotainer);
   });
 }
 
-/* Categories */
+/* Categories */ 
+   
 async function getCategoriesPreview() {
-  const {data} = await api("genre/movie/list"); //ya esta parseado
+/* const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_keu=' + API_KEY);
+  const data = await res.json()      .. con fetch
+ */
+// axios
+  const { data } = await api("genre/movie/list"); //ya esta parseado
 
   const categories = data.genres;
   categories.forEach((category) => {
     //                                         seleccionamos d section '#' dentro del secction la clase
-    const PreviewCategoriesContainer = document.querySelector(
-      "#categoriesPreview .categoriesPreview-list"
-    );
+    const categoriesPreviewList = document.querySelector("#categoriesPreview .categoriesPreview-list");
     const categoryContainer = document.createElement("div");
     categoryContainer.classList.add("category-container");
     const categoryTitle = document.createElement("h3");
@@ -56,11 +54,9 @@ async function getCategoriesPreview() {
 
     categoryTitle.appendChild(categoryTitleText);
     categoryContainer.appendChild(categoryTitle);
-    PreviewCategoriesContainer.appendChild(categoryContainer);
+    categoriesPreviewList.appendChild(categoryContainer);
   });
 }
 
 // getTrendingMoviesPreview();
 // getCategoriesPreview();
-
-
--- src/navigation.js
+++ src/navigation.js
@@ -1,3 +1,14 @@
+searchFormBtn.addEventListener('click', () => {
+  location.hash = '#search='
+});
+trendingBtn.addEventListener('click', () => {
+  location.hash = '#trends'
+});
+arrowBtn.addEventListener('click', () => {
+  location.hash = '#home'
+});
+
+
 window.addEventListener('DOMContentLoaded', navigator, false); // Carga el contenido al cargar la pagina
 window.addEventListener('hashchange', navigator, false); // Sirve para escuchar la petición
 
@@ -15,11 +26,31 @@ function navigator() { // vamos a llamar cada vez que cargue el navegador
   } else {
     homePage();
   }
+  /*  Operadores ternarios
+  
+  location.hash.startsWith('#trends')    ? trendsPage()       :
+  location.hash.startsWith('#search=')   ? searchPage()       :
+  location.hash.startsWith('#movie=')    ? movieDetailsPage() :
+  location.hash.startsWith('#category=') ? categoriesPage()   :
+  homePage() */
 }
 
 
 function homePage() {
   console.log('Home!!');
+
+  headerSection.classList.remove('header-container--long');
+  headerSection.style.background = '';
+  arrowBtn.classList.add('inactive');
+  arrowBtn.classList.remove('header-arrow--white');
+  headerTitle.classList.remove('inactive')
+  headerCategoryTitle.classList.add('inactive');
+  searchForm.classList.remove('inactive');
+
+  trendingPreviewSection.classList.remove('inactive')
+  categoriesPreviewSection.classList.remove('inactive');
+  genericSection.classList.add('inactive');
+  movieDetailSection.classList.add('inactive');
 
   getTrendingMoviesPreview();
   getCategoriesPreview();
@@ -27,17 +58,70 @@ function homePage() {
 
 function categoriesPage() {
   console.log('categories!!');
+
+  headerSection.classList.remove('header-container--long');
+  headerSection.style.background = '';
+  arrowBtn.classList.remove('inactive');
+  arrowBtn.classList.remove('header-arrow--white');
+  headerTitle.classList.add('inactive')
+  headerCategoryTitle.classList.remove('inactive');
+  searchForm.classList.add('inactive');
+
+  trendingPreviewSection.classList.add('inactive')
+  categoriesPreviewSection.classList.add('inactive');
+  genericSection.classList.remove('inactive');
+  movieDetailSection.classList.add('inactive');
 }
 
 function movieDetailsPage() {
   console.log('Movie!!');
+
+  headerSection.classList.add('header-container--long');
+  // headerSection.style.background = '';
+  arrowBtn.classList.remove('inactive');
+  arrowBtn.classList.add('header-arrow--white');
+  headerTitle.classList.add('inactive')
+  headerCategoryTitle.classList.add('inactive');
+  searchForm.classList.add('inactive');
+
+  trendingPreviewSection.classList.add('inactive')
+  categoriesPreviewSection.classList.add('inactive');
+  genericSection.classList.add('inactive');
+  movieDetailSection.classList.remove('inactive');
 }
 
 function searchPage() {
   console.log('Search!!');
+
+  headerSection.classList.remove('header-container--long');
+  // headerSection.style.background = '';
+  arrowBtn.classList.remove('inactive');
+  arrowBtn.classList.remove('header-arrow--white');
+  headerTitle.classList.add('inactive')
+  headerCategoryTitle.classList.remove('inactive');
+  searchForm.classList.remove('inactive');
+
+  trendingPreviewSection.classList.add('inactive')
+  categoriesPreviewSection.classList.add('inactive');
+  genericSection.classList.remove('inactive');
+  movieDetailSection.classList.add('inactive');
 }
 
 function trendsPage() {
   console.log('TRENDS!!');
+
+  headerSection.classList.remove('header-container--long');
+  // headerSection.style.background = '';
+  arrowBtn.classList.remove('inactive');
+  arrowBtn.classList.remove('header-arrow--white');
+  headerTitle.classList.add('inactive')
+  headerCategoryTitle.classList.remove('inactive');
+  searchForm.classList.add('inactive');
+
+  trendingPreviewSection.classList.add('inactive')
+  categoriesPreviewSection.classList.add('inactive');
+  genericSection.classList.remove('inactive');
+  movieDetailSection.classList.add('inactive');
+  
 }
 
